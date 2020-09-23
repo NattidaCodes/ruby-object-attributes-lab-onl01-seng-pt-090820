2bed6297968a34b0b008937f30654eeead082065
Nattida Wong <nattidac.wong@gmail.com>
2020-09-23 13:31:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,12 +2,18 @@ class Dog
   # def initialize(dog_name)
   #   @dog_name = dog_name
   # end
-  def name
-    @name
-  end
 
   def name=(dog_name)
     @dog_name = dog_name
   end
 
+  def name
+    @name
+  end
+
 end
+
+fido = Dog.new
+fido.dog_name = "Fido"
+
+fido.dog_name
